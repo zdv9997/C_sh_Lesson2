7854edce2d3cb93355ef1f79c74318e4e244ecfa
Dmitriy <zdv9997@mail.ru>
2022-11-18 14:12:20 +0200

    находим третью цифру числа

---
+++ zad13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = -1;
+if (num < 100)
+{
+    Console.WriteLine($"Число {num} не имеет третьей цифры");
+}
+else
+{
+    if (num > 999)
+    {
+        result = (num % 1000) / 100;
+        Console.WriteLine($"Третья цифра числа {num} является {result}");
+    }
+    else
+    {
+        result = num / 100;
+        Console.WriteLine($"Третья цифра числа {num} является {result}");
+    }
+}
+
+
