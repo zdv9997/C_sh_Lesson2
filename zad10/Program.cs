14dcdc3ac5bcc21528d94c83897e248c3d80c957
Dmitriy <zdv9997@mail.ru>
2022-11-18 09:46:31 +0200

    находим вторую цифру трехзначного числа

---
+++ zad10/Program.cs
@@ -0,0 +1,11 @@
+﻿while (true)
+{
+    Console.Write("Введите трехзначное число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num > 99 && num < 1000)
+    {
+        int n1 = (num % 100) / 10;
+        Console.Write($"Вторая цифра числа равняется: {n1}");
+        break;
+    }
+}
