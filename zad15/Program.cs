fce050c13a947244e2f96892407a8b2a55f2adf4
Dmitriy <zdv9997@mail.ru>
2022-11-18 14:30:36 +0200

    выясняем рабочий или нет день

---
+++ zad15/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите день недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 6 || num == 7) Console.WriteLine("это выходной день");
+else Console.WriteLine("Это рабочий день");
